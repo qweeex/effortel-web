e214dd75dddda5ce62547d05b166cbb6e0dee309
Arkady Pugachev <qweeex@yandex.com>
2023-05-26 10:32:30 +0300

    add main page

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <slot></slot>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import {defineNuxtConfig} from "nuxt/config";
 export default defineNuxtConfig({
     srcDir: 'src/',
     ssr: true,
+    css: [
+      '@/assets/style/reset.scss',
+      '@/assets/fonts/TT-Hoves/stylesheet.scss'
+    ],
     imports: {
         autoImport: true
     },
